a7a1aea811e5fb945543b68eb1e148d207018ccd
skashO <samueloshiorenua@gmail.com>
2023-02-17 23:27:29 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,11 +10,9 @@ int main(void)
     float floatType;
     double doubleType;
     char charType;
-
     printf("Size of int: %ld bytes\n", sizeof(integerType));
     printf("Size of float: %ld bytes\n", sizeof(floatType));
     printf("Size of double: %ld bytes\n", sizeof(doubleType));
     printf("Size of char: %ld byte\n", sizeof(charType));
-
     return (0);
 }
