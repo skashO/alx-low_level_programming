4921b0e67609f6f42c13c533098361edb0bd391e
skashO <samueloshiorenua@gmail.com>
2023-02-17 16:53:10 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,5 @@
-#include <stdio.io>
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n);
-	return(0);
+#include <stdio.h>
+int main() {
+   printf("with proper grammar, but the outcome is a piece of art,\n");
+   return 0;
 }
