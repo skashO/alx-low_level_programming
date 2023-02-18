105ae8b44f2ab2f6eaad3a981a0d56f0042a7343
skashO <samueloshiorenua@gmail.com>
2023-02-17 16:16:11 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,28 +1,16 @@
 #includes <stdio.h>
+#include <stdlib.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
 */
-int main(void)
-clrscr();
-	printf("    short int is %2d bytes \n", sizeof(short int));
-	printf("          int is %2d bytes \n", sizeof(int));
-	printf("        int * is %2d bytes \n", sizeof(int *));
-	printf("     long int is %2d bytes \n", sizeof(long int));
-	printf("   long int * is %2d bytes \n", sizeof(long int *));
-	printf("   signed int is %2d bytes \n", sizeof(signed int));
-	printf(" unsigned int is %2d bytes \n", sizeof(unsigned int));
-	printf("\n");
-	printf("        float is %2d bytes \n", sizeof(float));
-	printf("      float * is %2d bytes \n", sizeof(float *));
-	printf("       double is %2d bytes \n", sizeof(double));
-	printf("     double * is %2d bytes \n", sizeof(double *));
-	printf("  long double is %2d bytes \n", sizeof(long double));
-	printf("\n");
-	printf("  signed char is %2d bytes \n", sizeof(signed char));
-	printf("         char is %2d bytes \n", sizeof(char));
-	printf("       char * is %2d bytes \n", sizeof(char *));
-	printf("unsigned char is %2d bytes \n", sizeof(unsigned char));
-	getch();
+int main()
+{
+	printf("size of char is=%i byte\n", sizeof(char));
+	printf("size of int is=%i byte\n", sizeof(int));
+	printf("size of long int is=%i byte\n", sizeof(long int));
+	printf("size of long long int is=%i byte\n", sizeof(long long int));
+	printf("size of float is=%i byte\n", sizeof(float));
+	return (0);
 }
